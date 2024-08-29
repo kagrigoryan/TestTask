6755bc3436040556ee60f107888cd3c17c76e6d3
kagrigoryan <chantofpaagro@gmail.com>
2024-08-29 18:59:34 +0300

    vue3-V1

--- frontend/src/components/TaskList.vue
+++ frontend/src/components/TaskList.vue
@@ -3,26 +3,26 @@
         <div class="header">
             <h1>Task Tracker</h1>
             <div class="tabs">
-                <button :class="{ active: activeTab === 'active' }" @click="activeTab = 'active'">Active</button>
-                <button :class="{ active: activeTab === 'completed' }" @click="activeTab = 'completed'">Completed</button>
+                <button :class="{ active: activeTab === 'active' }" @click="activeTab = 'active'">Текущие задачи</button>
+                <button :class="{ active: activeTab === 'completed' }" @click="activeTab = 'completed'">Выполненные</button>
             </div>
             <form v-if="activeTab === 'active'" @submit.prevent="addTask">
-                <input type="text" v-model="newTask" placeholder="Add a new task" required>
-                <button type="submit">Add Task</button>
+                <input type="text" v-model="newTask" placeholder="Добавьте свою задачу" required>
+                <button type="submit"><i class="fas fa-tasks"></i></button>
             </form>
         </div>
         <ul>
             <li v-for="task in filteredTasks" :key="task.id" :class="{ completed: task.completed }">
                 <button @click="toggleTaskCompletion(task)">
-                    <i v-if="task.completed" class="fas fa-check-circle"></i>
-                    <i v-else class="far fa-circle"></i>
+                    <i v-if="task.completed" class="far fa-circle"></i>
+                    <i v-else class="fas fa-check"></i>
                 </button>
-                <span v-if="!task.editing" class="task-text" @click="startEditing(task)">{{ task.title }}</span>
+                <span v-if="!task.editing" class="task-text">{{ task.title }}</span>
                 <input v-else type="text" class="task-edit-input" v-model="task.title" @blur="saveEditing(task)" @keydown.enter="saveEditing(task)">
                 <div>
-                    <button v-if="!task.editing" @click="startEditing(task)"><i class="fas fa-edit"></i></button>
-                    <button v-else @click="saveEditing(task)"><i class="fas fa-check"></i></button>
-                    <button @click="deleteTask(task.id)"><i class="fas fa-trash"></i></button>
+                    <button v-if="!task.editing" @click="startEditing(task)" class="edit-button"><i class="fas fa-edit"></i></button>
+                    <button v-else @click="saveEditing(task)" class="save-button"><i class="fas fa-save"></i></button>
+                    <button @click="deleteTask(task.id)" class="delete-button"><i class="fas fa-trash"></i></button>
                 </div>
             </li>
         </ul>
